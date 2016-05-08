61f2bb5b60c33df0ad32bd0ba107f25eeeb84445
SmeeK153 <eric.freeman@ago.org>
2016-05-08 07:23:54 +0000

    added support for generic views

--- polls/models.py
+++ polls/models.py
@@ -7,7 +7,8 @@ from django.utils.encoding import python_2_unicode_compatible
 
 class Question(models.Model):
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     def __str__(self):
         return self.question_text
     question_text = models.CharField(max_length=200)
